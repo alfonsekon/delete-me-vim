d848bdc49eae77487a4fffc9dec2f636ef70213e
alfonsekon <agdelosreyes1@up.edu.ph>
2025-03-10 17:56:28 +0800

    feat: countdown timer

--- modes/relativeLineJump.js
+++ modes/relativeLineJump.js
@@ -1,20 +1,65 @@
 const vscode = require("vscode");
-const { jumpToNewFile, spawnText, createNewFile } = require('../utils/utils');
+const { deleteFile, jumpToNewFile, spawnText, createNewFile } = require('../utils/utils');
+
 const lineCount = 15;
+let MAIN_MESSAGE = 'Delete Me!';
+
 let score = 0;
-let statusBarItem;
+let scoreStatusbarItem;
+
+let countdown = 15;
+let countdownInterval;
+let countdownStatusBarItem;
+
+async function endGame() {
+    if (countdownInterval) {
+        clearInterval(countdownInterval);
+    }
+    vscode.window.showErrorMessage(`Time is up! Your score was ${score}`);
+}
+
+function startCountdown() {
+    if (countdownInterval) {
+        clearInterval(countdownInterval);
+    }
+
+    countdown = 15;
+    updateCountdownDisplay();
+
+    countdownInterval = setInterval(() => {
+        countdown--;
+        updateCountdownDisplay();
+
+        if (countdown <= 0) {
+            clearInterval(countdownInterval);
+            endGame();
+        }
+    }, 1000)
+}
+
+async function updateCountdownDisplay() {
+    if (!countdownStatusBarItem) {
+        countdownStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+        countdownStatusBarItem.show();
+    }
+    countdownStatusBarItem.text = `Time Remaining: ${countdown}s`;
+    countdownStatusBarItem.tooltip = `Delete Me! Vim - Relative Line Jump time remaining`
+}
 
 async function updateScore() {
     score++;
-    statusBarItem.text = `Line Jump Score: ${score}`;
-    statusBarItem.show();
+    scoreStatusbarItem.text = `Line Jump Score: ${score}`;
+    scoreStatusbarItem.show();
 }
 
-async function showStatusBar() {
-    statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-    statusBarItem.text = `Line Jump Score: 0`;
-    statusBarItem.tooltip = `Delete Me! Vim - Relative Line Jump score`;
-    statusBarItem.show();
+async function updateScoreDisplay() {
+    if (!scoreStatusbarItem) {
+        scoreStatusbarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+    }
+    score = 0;
+    scoreStatusbarItem.text = `Line Jump Score: ${score}`;
+    scoreStatusbarItem.tooltip = `Delete Me! Vim - Relative Line Jump score`;
+    scoreStatusbarItem.show();
 }
 
 async function spawnNLines(fileUri, numOfLines) {
@@ -33,7 +78,12 @@ async function countDocumentLines(editor) {
     return { totalLines, document };
 }
 
-async function spawnDeleteMe(fileUri, customMsg = 'Delete Me!') {
+async function spawnScore(fileUri, score) {
+    const congratsMsg = `Congratulations! You were able to delete ${score} lines in ${countdown} seconds!`
+    const pbMsg = `Personal Best: `
+}
+
+async function spawnDeleteMe(fileUri, customMsg = MAIN_MESSAGE) {
     customMsg += '\n';
     const editor = vscode.window.activeTextEditor;
     if (!editor) return;
@@ -46,7 +96,7 @@ async function spawnDeleteMe(fileUri, customMsg = 'Delete Me!') {
         editBuilder.insert(new vscode.Position(lineNumber, 0), customMsg);
     })
 
-    // vscode.window.showInformationMessage(`Spawned '${customMsg}' on line ${lineNumber + 1}.`); //+1 for 1 index
+    // vscode.window.showInformationMessage(`Spawned '${customMsg}' on line ${ lineNumber + 1 }.`); //+1 for 1 index
     await deleteListener(document, customMsg, lineNumber);
 }
 
@@ -57,16 +107,16 @@ async function checkMsg(document, targetMsg, lineNumber) {
     }
 
     const line = document.lineAt(lineNumber);
-    console.log(`Content at line ${lineNumber} is ${line.text}`);
+    console.log(`Content at line ${lineNumber} is ${line.text} `);
     return (line.text).length !== 0;
 }
 
 async function deleteListener(document, targetMsg, lineNumber) {
     const disposable = vscode.workspace.onDidChangeTextDocument(async event => {
-        // console.log(`is correct document?: ${event.document === document}`);
+        // console.log(`is correct document ?: ${ event.document === document } `);
         if (event.document === document) {
             const stillExists = await checkMsg(document, targetMsg, lineNumber);
-            // console.log(`stillExists?: ${stillExists}`);
+            // console.log(`stillExists ?: ${ stillExists } `);
             if (!stillExists) {
                 await updateScore();
                 disposable.dispose();
@@ -84,7 +134,9 @@ async function relativeLineJump(context) {
             const fileUri = await createNewFile('delete-me-vim|relative-line-jump');
             await spawnNLines(fileUri, lineCount);
             await jumpToNewFile(fileUri);
-            await showStatusBar();
+            await updateScoreDisplay();
+            await updateCountdownDisplay();
+            startCountdown();
             await spawnDeleteMe(fileUri);
         })
     );

5dcc667585dfc01e468e213c546c3d82ceffeec7
alfonsekon <agdelosreyes1@up.edu.ph>
2025-03-12 00:57:53 +0800

    remove test

--- extension.js
+++ extension.js
@@ -1,11 +1,9 @@
 const { deleteFiles } = require('./utils/utils');
-const { testExtension } = require('./modes/test');
 const { relativeLineJump } = require('./modes/relativeLineJump');
 
 async function activate(context) {
     console.log("delete-me-vim is active!");
 
-    await testExtension(context);
     await relativeLineJump(context);
 
 }
